489ee938f675fad0ca2071dd3c98a3c8aca46706
Christina Bateman <chrmhint@ut.utm.edu>
2019-02-22 21:13:24 -0600

    just the proj1.py with comments
    Please double-check for accuracy :)

--- proj1.py
+++ proj1.py
@@ -4,35 +4,52 @@
 
 import os, fnmatch, bs4, string, nltk
 
+#remove .DS_Store from each directory
 def filter(toFilter):
     for i in toFilter:
         if i == ".DS_Store":
             toFilter.remove(i)
     return toFilter
 
+#update word frequency and add to wordList in correct format
 class wordTrack:
+    #param. constructor
     def __init__(self, wordAdd, wordNum):
         self.word = wordAdd
         self.num = int(wordNum)
+
+    #sorting rule
+    #return true if object's word frequency is less than rh object's
     def __lt__(self, other):
         return self.num < other.num
+
+    #return object information
     def __repr__(self):
         return (self.word + " : " + str(self.num))
+    
     def __str__(self):
         return (self.word+ " : " + str(self.num))
 
-
+#list of words from file
 tokens = []
+#stemming method
 porter = nltk.stem.PorterStemmer()
+
+#remove .DS_Store from each directory
 dirs1 = filter(os.listdir("webkb"))
 allText = ""
 if os.path.isdir('./noTags') == False:
     os.mkdir('./noTags')
+
+#open stopwords.txt and get list of stop words
 stopFile = open("stopWords.txt", 'r')
 tempStop = stopFile.read()
 stopWords = set(tempStop.split())
 stopFile.close()
 table = str.maketrans({key: " " for key in string.punctuation})
+
+#go through directories
+#remove html
 for i in dirs1:
     pathPt1 = "./noTags/" + i
     if os.path.isdir(pathPt1) == False:
@@ -58,18 +75,35 @@ for i in dirs1:
                 w = open(pathPt3, "w")
                 w.write(text)
                 w.close()
+
+#separate each word from allText and move each to tokens[]               
 allText = allText.translate(table)
 allText = allText.lower()
 tokens = nltk.tokenize.word_tokenize(allText)
+
+#list of stemmed words
 filteredTokens = []
+
+#stem every word that is not a stop word
+#add stemmed word to filteredTokens[]
 for x in tokens:
     if x not in stopWords and x.isdigit() == False :
         x = porter.stem(x)
         filteredTokens.append(x)
+
+#count frequency of each stemmed word       
 freq = nltk.FreqDist(filteredTokens)
 wordList = []
+
+#for each pair in list of frequencies:
+#add each pair to WordList[]
+#this attaches the frequency (value) to the word (key)
 for key,val in freq.items():
     wordList.append(wordTrack(str(key), str(val)))
+
+#sort wordList[]
 wordList = sorted(wordList)
+
+#print results
 for x in wordList:
     print(x)
